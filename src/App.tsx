fad2af1f3fbe400fce4f0d9f224bf2ea7eec18c6
arlok <karlo.peric.rix@gmail.com>
2025-08-21 21:27:01 +0200

    okej zadnja poruka je bila cudna malo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-
 import "./App.css";
 import Nav from "./Nav";
+import hero from "./assets/team_flag_image.jpg";
 function App() {
   return (
     <>
       <Nav />
       <div className="about">
-        <div>
-          <h1>Crobotics #7201</h1>
-          <h5>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eveniet
-            doloremque unde repellendus inventore optio. Sed iure quas error
-            quae
-          </h5>
+        <div className="hero-text">
+          <h1>
+            CR<span className="third-letter">O</span>botics #7201
+          </h1>
+          <p>
+            A small team of high school students aiming to compete <br />
+            at a high level with a tight budget.
+          </p>
         </div>
+        <img src={hero} alt="Crobotics Team" className="hero-image" />
       </div>
     </>
   );
